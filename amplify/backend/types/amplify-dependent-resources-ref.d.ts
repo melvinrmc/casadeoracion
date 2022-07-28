2d001dbfbfef6900eb9a6e106caeccc836aed1e5
Melvin Ruben Miculax Coc <mrmiculax@tigo.com.gt>
2022-07-28 02:55:14 -0600

    upd local

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "casadeoracionfcc8753d": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
