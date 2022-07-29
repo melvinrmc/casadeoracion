7f14849ee34b4a345cf72270d9834048da6a2acc
melvinrmc <melvinrmc@hotmail.com>
2022-07-29 00:16:22 -0600

    ADD authenticated options rules

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { Amplify } from 'aws-amplify';
+import { AuthenticatorService } from '@aws-amplify/ui-angular';
+import awsExports from '../aws-exports';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+  styleUrls: ['./top-bar.component.css'],
 })
 export class TopBarComponent {
-
+  constructor(public authenticator: AuthenticatorService) {
+    Amplify.configure(awsExports);
+  }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
