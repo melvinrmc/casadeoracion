e5f81640e6a66aa2fa9955ce899c2a8b52577849
melvinrmc <melvinrmc@hotmail.com>
2022-08-17 23:53:09 -0600

    UPD show user data and email from cognito

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Auth } from 'aws-amplify';
+
+Auth.currentAuthenticatedUser({
+  bypassCache: false, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+})
+  .then((user) => console.log(user))
+  .catch((err) => console.log(err));
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
