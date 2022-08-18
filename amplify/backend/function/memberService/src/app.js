0c7f13c670e35f243e57fc1f8e49f7a883adf78f
Melvin Ruben Miculax Coc <mrmiculax@tigo.com.gt>
2022-08-18 13:19:51 -0600

    UPD Migrated successfully

--- amplify/backend/function/memberService/src/app.js
+++ amplify/backend/function/memberService/src/app.js
@@ -23,7 +23,7 @@ if (process.env.ENV && process.env.ENV !== "NONE") {
 }
 
 const userIdPresent = false; // TODO: update in case is required to use that definition
-const partitionKeyName = "register.id";
+const partitionKeyName = "id";
 const partitionKeyType = "S";
 const sortKeyName = "";
 const sortKeyType = "";
